7686ec33f972e0630222b4c76da3ee3fa209d08f
Maxwell-ferreira-web <maxwellferreirabh@yahoo.com.br>
2021-08-27 18:42:01 -0300

    Primeiro commit do Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Logo from './Logo.png';
 import './App.css';
 
-function App() {
+class App extends Component {
+ render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="Entrar">Entrar</p>
+      <p className="Cadastro">Cadastro</p>
+      <p className="Login">Login</p>
+      <p className="Linha1">___________________________________</p>
+      <p className="Senha">Senha</p>
+      <p className="Linha2">___________________________________</p>
+      <p className="Esqueceu">Esqueceu a senha?</p>
+      <button className="Button">Entrar</button>
+      <img src={Logo} className="Image" alt="Logo" />
+      </header>      
     </div>
   );
+}
 }
 
 export default App;
